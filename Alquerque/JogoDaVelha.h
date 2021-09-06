28c4e21849ca7c762b0283fb959b7aa37f3cf23d
Victor Scherer Putrich <scherer.victor98@gmail.com>
2021-09-06 16:51:21 -0300

    Update JogoDaVelha.h

--- Alquerque/JogoDaVelha.h
+++ Alquerque/JogoDaVelha.h
@@ -5,18 +5,12 @@
 class JogoDaVelha
 {
 private:
-	enum INTERNAL_STATE{
-		INSERT,
-		MOVE
-	};
-	int pieces_left_p1 = 0;
-	int pieces_left_p2 = 0;
-	INTERNAL_STATE current_state_p1 = INSERT;
-	INTERNAL_STATE current_state_p2 = INSERT;
+	map<int,int> pieces_left{ {-1,0}, {1,0} };
 public:
 	
 	stack<Movement> move_pile; //PRECISA SER UMA PILHA!!!
-	JogoDaVelha() {} //pedir número de peças e número de peças não colocadas como parâmetro
+	JogoDaVelha() {} 
+	JogoDaVelha(int number_pieces);
 	vector<Movement> possibleMoves(Tabuleiro &alquerque, const Player &mark);
 	bool makeMove(Tabuleiro &alquerque, Movement &movement);
 	void UndoMove(Tabuleiro &alquerque); // pilhaaaaaaaaa
